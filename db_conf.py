5ae62da01c26e7766f948b3958a3385af3097ff9
artturig <artturi@saunalahti.fi>
2016-02-15 18:24:52 +0000

    remove conf and pyc from repo

--- db_conf.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-
-def dbconn():
-	db = MySQLdb.Connect(host="localhost", # your host, usually localhost
-        user="root", # your username
-        passwd="", # your password
-        db="raspi") # name of the data base
-
-dbconn()
