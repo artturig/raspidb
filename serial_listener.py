f51322663305b4feb7c1874f99a6cf368b8ad182
artturig <artturi@saunalahti.fi>
2016-05-14 19:31:02 +0000

    reading serial, write temperature to db, changed config files to .config.ini

--- serial_listener.py
+++ serial_listener.py
@@ -1,37 +1,48 @@
 import serial
-import MySQLdb
 import sys
-import db_conf
+import MySQLdb
 
-# use dbconn connection settings from db_conf.py file
-db_conf.dbconn()
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser  # ver. < 3.0
+
+config = ConfigParser()  # instantiate
+config.read('mysql.config.ini')  # parse existing file
+# read values from a mysql.config.ini
+myhost = config.get('mysql', 'host')
+myuser = config.get('mysql', 'user')
+mypasswd = config.get('mysql', 'passwd')
+mydb = config.get('mysql', 'db')
 
 # check that we  have usb connected
 try:
-	serialport = serial.Serial("/dev/ttyUSB0", 57600, timeout=0.5)
+    serialport = serial.Serial("/dev/ttyUSB0", 57600, timeout=0.5)
 except Exception, e:
-	print >> sys.stderr, "Exception: %s" % str(e)
-	print >> sys.stderr, "No valid usb found from dev"
-	sys.exit(1)
+    print >> sys.stderr, "Exception: %s" % str(e)
+    print >> sys.stderr, "No valid usb found from dev"
+    sys.exit(1)
+
+# use config settings to db connection
+db = MySQLdb.Connect(host=myhost,  # your host, usually localhost
+                     user=myuser,  # your username
+                     passwd=mypasswd,  # your password
+                     db=mydb)  # name of the data base
 
 cur = db.cursor()
 
-while True:    
-	command = serialport.readline()
-	print command
-	s = command.strip()
-#	s = s.replace(".", ",")
-
-#	query = "SELECT lampo from valo1"
-#	cur.execute(query)
-#	ver = cur.fetchone()
-
-#	print "luku on %s " % ver
-#	query = "INSERT INTO valo1 SET lampo = 1"
-	if not s:
-		print "tyhja"
-	else:	
-		query = "INSERT INTO valo1 SET lampo = %s " % (s)
-		print query
-		cur.execute(query)
-		db.commit()
+# loop
+while True:
+    command = serialport.readline()
+    print command
+    s = command.strip()
+    suffix = ";"
+    if s.endswith(suffix) == False:  # in line is not ending with ; forget it
+        print "crap"
+    elif not s:  # if s is empty forget it
+        print "tyhja"
+    else:
+        s = s[:-1]  # remove tailing ;
+        node, value = s.split(":")  # split message and assing variables
+        cur.execute("INSERT INTO lampo1 (node, lampo)  VALUES (%s, %s) ", (node, value))
+        db.commit()
